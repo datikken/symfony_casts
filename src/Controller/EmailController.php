5eeb649ad29369005a02c2d9904f01ee2df613b9
nshadybekov <nshadybekov@adsterra.com>
2022-03-07 01:21:17 +0300

    assets versioning email images

--- src/Controller/EmailController.php
+++ src/Controller/EmailController.php
@@ -2,22 +2,22 @@
 
 namespace App\Controller;
 
+use Symfony\Bridge\Twig\Mime\TemplatedEmail;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Mailer\MailerInterface;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\Mime\Email;
 
 class EmailController extends AbstractController
 {
     #[Route('/email/send', name: 'test_email')]
     public function index(MailerInterface $mailer): Response
     {
-        $email = (new Email())
+        $email = (new TemplatedEmail())
             ->from('tikkenn@yandex.ru')
             ->to('tikken23@gmail.com')
             ->subject('Nice to meet you')
-            ->text('Ya ya!');
+            ->htmlTemplate('email/index.html.twig');
 
         $mailer->send($email);
 
--- webpack.config.js
+++ webpack.config.js
@@ -54,7 +54,10 @@ Encore
         config.useBuiltIns = 'usage';
         config.corejs = 3;
     })
-
+    .copyFiles({
+        from: './assets/images',
+        to: 'images/[path][name].[hash:8].[ext]'
+    })
     // enables Sass/SCSS support
     //.enableSassLoader()
 
